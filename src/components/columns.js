f37ae5de5bcfcc8d2e2fd26383643d2cd5564557
vimalAtLR <vimal@logicrays.com>
2023-04-17 15:02:24 +0530

    headers, footers, body content render from json data wirh react-table library

---
+++ src/components/columns.js
@@ -0,0 +1,42 @@
+export const COLUMNS = [
+    {
+        Header: "Id",
+        Footer: "Id",
+        accessor: "id",
+    },
+    {
+        Header: "First Name",
+        Footer: "First Name",
+        accessor: "first_name",
+    },
+    {
+        Header: "Last Name",
+        Footer: "Last Name",
+        accessor: "last_name",
+    },
+    {
+        Header: "Email",
+        Footer: "Email",
+        accessor: "email",
+    },
+    {
+        Header: "Date of Birth",
+        Footer: "Date of Birth",
+        accessor: "date_of_birth",
+    },
+    {
+        Header: "Age",
+        Footer: "Age",
+        accessor: "age",
+    },
+    {
+        Header: "Country",
+        Footer: "Country",
+        accessor: "country",
+    },
+    {
+        Header: "Phone",
+        Footer: "Phone",
+        accessor: "phone",
+    },
+]
